2886c5d70c58095ef87ef861c6356b2ccffe70ef
futurebound <mitchellm1618@gmail.com>
2025-01-15 18:26:06 -0800

    feat: sign up new user

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcryptjs')
 const dotenv = require('dotenv')
 dotenv.config()
+
+const path = require('node:path')
 
 /**
  * ------------- EXPRESS SETUP -----------
@@ -8,6 +11,43 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+/**
+ * ------------- POSTGRES SETUP -----------
+ */
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DB_URI,
+})
+
+/**
+ * ---------------- VIEWS ----------------
+ */
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+/**
+ * ---------------- ROUTES ----------------
+ */
+app.get('/signup', (req, res) => res.render('signUpForm'))
+app.post('/signup', async (req, res, next) => {
+  try {
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err)
+      }
+    })
+    const { firstName, lastName, email, password } = req.body
+    await pool.query(
+      'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)',
+      [firstName, lastName, email, password],
+    )
+    res.json({ message: 'User created successfully ' })
+  } catch (err) {
+    return next(err)
+  }
+})
 
 /**
  *  ---------------- SERVER ---------------
